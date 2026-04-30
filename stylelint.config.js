b9b9d010f14631025e80166599002c8ac83b8e97
Ali <muhammed.a.1214@gmail.com>
2026-04-30 23:40:21 +0500

    project structure setup

---
+++ stylelint.config.js
@@ -0,0 +1,10 @@
+{
+  "tagname-lowercase": true,
+  "attr-lowercase": true,
+  "attr-value-double-quote": true,
+  "doctype-first": true,
+  "tag-pair": true,
+  "id-unique": true,
+  "src-not-empty": true,
+  "alt-require": true
+}
